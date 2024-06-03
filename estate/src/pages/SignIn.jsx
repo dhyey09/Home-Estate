2e0c293ae3bd872329da66399cc8dfd5f5384e03
Dhyey <dhyeythakkar0901@gmail.com>
2024-06-03 18:03:00 +0530

    Sign in Complete

--- estate/src/pages/SignIn.jsx
+++ estate/src/pages/SignIn.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react';
+import { FaEye } from 'react-icons/fa';
 
 export default function SignIn() {
+    const [formData, setFormData] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [visible, setVisible] = useState(false);
+    const navigate = useNavigate();
+
+    const handleVisible = (e) => {
+        e.preventDefault();
+        setVisible(!visible);
+    }
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value,
+        });
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        const res = await fetch('/api/auth/sign-in',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            }
+        );
+        const data = await res.json();
+        if (data.success === false) {
+            setLoading(false);
+            alert(data.message);
+        }
+        else {
+            setError(null);
+            navigate('/')
+        }
+    }
     return (
-        <div>Sign In</div>
+        <div className='p-3 max-w-lg mx-auto'>
+            <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+            <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+                <input type="text" placeholder='email' className='border p-3 rounded-lg ' id='email' onChange={handleChange} required />
+                <span className="flex items-center">
+                    <input type={visible ? 'text' : 'password'} placeholder='password' className='border p-3 rounded-l-lg w-full' id='password' onChange={handleChange} required />
+                    <button className="bg-red-500 p-4 rounded-r-lg hover:bg-red-600" onClick={handleVisible}><FaEye className='text-slate-200' /></button>
+                </span>
+                <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80">Sign in</button>
+            </form>
+            <div className='flex gap-2 mt-2'>
+                <p>Don't have an account?</p>
+                <Link to={"/sign-up"}><span className='text-blue-700'>Sign up</span></Link>
+            </div>
+        </div>
     )
 }
--- estate/src/pages/SignUp.jsx
+++ estate/src/pages/SignUp.jsx
@@ -1,10 +1,22 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react';
+import { FaEye } from 'react-icons/fa';
 
 export default function SignUp() {
-    const [ formData, setFormData ] = useState({});
-    const [ error, setError ] = useState(null);
-    const [ loading, setLoading ] = useState(false);
+    const [formData, setFormData] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [visible1, setVisible1] = useState(false);
+    const [visible2, setVisible2] = useState(false);
+    const navigate = useNavigate();
+
+    const handleVisible1 = (e) => {
+        e.preventDefault();
+        setVisible1(!visible1);
+    }
+    const handleVisible2 = (e) => {
+        e.preventDefault();
+        setVisible2(!visible2);
+    }
     const handleChange = (e) => {
         setFormData({
             ...formData,
@@ -28,22 +40,32 @@ export default function SignUp() {
                 }
             );
             const data = await res.json();
-            if(data.success===false){
-                setError(data.message);
+            if (data.success === false) {
                 setLoading(false);
+                alert(data.message);
             }
+            else {
+                setError(null);
+                navigate('/sign-in')
+            }
+
         }
-        setError(null);
     }
     return (
         <div className='p-3 max-w-lg mx-auto'>
             <h1 className="text-3xl text-center font-semibold my-5">Sign Up</h1>
             <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-                <input type="text" placeholder='username' className='border p-3 rounded-lg ' id='username' onChange={handleChange} required/>
-                <input type="text" placeholder='email' className='border p-3 rounded-lg ' id='email' onChange={handleChange} required/>
-                <input type="text" placeholder='phone' className='border p-3 rounded-lg ' id='phone' onChange={handleChange} required/>
-                <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password1' onChange={handleChange} required/>
-                <input type="password" placeholder='confirm password' className='border p-3 rounded-lg' id='password2' onChange={handleChange} required/>
+                <input type="text" placeholder='username' className='border p-3 rounded-lg ' id='username' onChange={handleChange} required />
+                <input type="text" placeholder='email' className='border p-3 rounded-lg ' id='email' onChange={handleChange} required />
+                <input type="text" placeholder='phone' className='border p-3 rounded-lg ' id='phone' onChange={handleChange} required />
+                <span className="flex items-center">
+                    <input type={visible1 ? 'text' : 'password'} placeholder='password' className='border p-3 rounded-l-lg w-full' id='password1' onChange={handleChange} required />
+                    <button className="bg-red-500 p-4 rounded-r-lg hover:bg-red-600" onClick={handleVisible1}><FaEye className='text-slate-200' /></button>
+                </span>
+                <span className="flex items-center">
+                    <input type={visible2 ? 'text' : 'password'} placeholder='confirm password' className='border p-3 rounded-l-lg w-full' id='password2' onChange={handleChange} required />
+                    <button className="bg-red-500 p-4 rounded-r-lg hover:bg-red-600" onClick={handleVisible2}><FaEye className='text-slate-200' /></button>
+                </span>
                 <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80">Sign up</button>
             </form>
             <div className='flex gap-2 mt-2'>
